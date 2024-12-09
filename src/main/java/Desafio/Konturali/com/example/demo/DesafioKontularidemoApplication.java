ef513cbc209ee0944c543d57b3694f631ffb926f
Ruan Vinicios <rvruanvin@gmail.com>
2024-12-09 19:40:06 -0300

    dealterações pendentes

---
+++ src/main/java/Desafio/Konturali/com/example/demo/DesafioKontularidemoApplication.java
@@ -0,0 +1,13 @@
+package Desafio.Konturali.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKontularidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKontularidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Desafio/Konturali/com/example/demo/DesafioKontularidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio.Konturali.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioKontularidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
